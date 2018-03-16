aaa078a857b4aec4295bf61987b1c19bfbf14eaf
Simon Ho <simonsays87@googlemail.com>
2018-03-15 23:42:40 -0700

    Mock modules for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+from mock import Mock as MagicMock
+ 
+class Mock(MagicMock):
+@classmethod
+def __getattr__(cls, name):
+return MagicMock()
+ 
+MOCK_MODULES = ['numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
