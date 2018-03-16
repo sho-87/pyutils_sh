99419c028dd7b4bc10e45df4295e040b68e11b6d
Simon Ho <simonsays87@googlemail.com>
2018-03-15 23:46:23 -0700

    Fix indentation

--- docs/conf.py
+++ docs/conf.py
@@ -5,9 +5,9 @@ sys.path.insert(0, os.path.abspath('../'))
 from mock import Mock as MagicMock
  
 class Mock(MagicMock):
-@classmethod
-def __getattr__(cls, name):
-return MagicMock()
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
  
 MOCK_MODULES = ['numpy', 'pandas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
