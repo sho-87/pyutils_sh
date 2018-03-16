8d7af06f5ebbd3dde438ea7bd88d5d6a43e4229e
Simon Ho <simonsays87@googlemail.com>
2018-03-16 00:08:52 -0700

    Add pandas to mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
  
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy', 'pandas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
